a7b55803b374ba821cc97bbca37e0cffe5bf2ef8
Jose Carlos <rodjosecarlos15@gmail.com>
2024-06-22 18:21:18 -0700

    UPDATE pos e index

--- pos.sql
+++ pos.sql
@@ -2,14 +2,16 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 22, 2024 at 05:46 PM
--- Server version: 8.3.0
--- PHP Version: 8.2.18
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-06-2024 a las 19:39:31
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -18,30 +20,36 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pos`
+-- Base de datos: `pos2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clientes`
+-- Estructura de tabla para la tabla `clientes`
 --
 
-DROP TABLE IF EXISTS `clientes`;
-CREATE TABLE IF NOT EXISTS `clientes` (
-  `id_cliente` int NOT NULL,
-  `membresia` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `sexo` varchar(255) NOT NULL,
+CREATE TABLE `clientes`
+(
+  `id_cliente` int
+(11) NOT NULL,
+  `memebresia` varchar
+(255) NOT NULL,
+  `sexo` varchar
+(255) NOT NULL,
   `fecha_nacimiento` date NOT NULL,
-  `correo` varchar(255) NOT NULL,
-  UNIQUE KEY `correo` (`correo`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `correo` varchar
+(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `clientes`
+-- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`id_cliente`, `membresia`, `sexo`, `fecha_nacimiento`, `correo`) VALUES
+INSERT INTO `clientes` (`
+id_cliente`,
+`memebresia
+`, `sexo`, `fecha_nacimiento`, `correo`) VALUES
 (12530, 'Platinum', 'Femenino', '2003-08-20', 'roblesangie@gmail.com'),
 (23456, 'Oro', 'Maculino', '1970-11-23', 'felixr@gmail.com'),
 (97843, 'Diamante', 'Femenino', '1968-08-20', 'luzanilla@gmail.com'),
@@ -51,23 +59,29 @@ INSERT INTO `clientes` (`id_cliente`, `membresia`, `sexo`, `fecha_nacimiento`, `
 -- --------------------------------------------------------
 
 --
--- Table structure for table `productos`
+-- Estructura de tabla para la tabla `productos`
 --
 
-DROP TABLE IF EXISTS `productos`;
-CREATE TABLE IF NOT EXISTS `productos` (
-  `codigo` varchar(10) NOT NULL,
-  `nombre_producto` varchar(255) NOT NULL,
-  `precio` float(9,2) NOT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  UNIQUE KEY `codigo` (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `productos`
+(
+  `codigo` varchar
+(10) NOT NULL,
+  `nombre_producto` varchar
+(255) NOT NULL,
+  `precio` float
+(9,2) NOT NULL,
+  `descripcion` varchar
+(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `productos`
+-- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`codigo`, `nombre_producto`, `precio`, `descripcion`) VALUES
+INSERT INTO `productos` (`
+codigo`,
+`nombre_producto
+`, `precio`, `descripcion`) VALUES
 ('P001', 'Manzana', 10.99, NULL),
 ('P002', 'Banana', 15.49, NULL),
 ('P003', 'Naranja', 7.99, NULL),
@@ -82,21 +96,27 @@ INSERT INTO `productos` (`codigo`, `nombre_producto`, `precio`, `descripcion`) V
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_venta` varchar(255) NOT NULL,
-  `apellido` varchar(255) NOT NULL,
-  `sexo` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE `usuarios`
+(
+  `id_venta` varchar
+(255) NOT NULL,
+  `apellido` varchar
+(255) NOT NULL,
+  `sexo` varchar
+(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_venta`, `apellido`, `sexo`) VALUES
+INSERT INTO `usuarios` (`
+id_venta`,
+`apellido
+`, `sexo`) VALUES
 ('Rodolfo', 'Marquez', 'Masculino'),
 ('Jose', 'Ramirez', 'Masculino'),
 ('Julia', 'Nuñez', 'Femenino'),
@@ -106,57 +126,112 @@ INSERT INTO `usuarios` (`id_venta`, `apellido`, `sexo`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ventas`
+-- Estructura de tabla para la tabla `ventas`
 --
 
-DROP TABLE IF EXISTS `ventas`;
-CREATE TABLE IF NOT EXISTS `ventas` (
-  `id_venta` int UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ventas`
+(
+  `id_venta` int
+(10) UNSIGNED NOT NULL,
   `hora_venta` time NOT NULL,
-  `fecha_venta` date NOT NULL,
-  PRIMARY KEY (`id_venta`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `fecha_venta` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `ventas`
+-- Volcado de datos para la tabla `ventas`
 --
 
-INSERT INTO `ventas` (`id_venta`, `hora_venta`, `fecha_venta`) VALUES
+INSERT INTO `ventas` (`
+id_venta`,
+`hora_venta
+`, `fecha_venta`) VALUES
 (1, '09:48:13', '2024-06-01'),
 (2, '09:48:37', '2024-06-01');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `venta_detalle`
+-- Estructura de tabla para la tabla `venta_detalle`
 --
 
-DROP TABLE IF EXISTS `venta_detalle`;
-CREATE TABLE IF NOT EXISTS `venta_detalle` (
-  `id_venta` int UNSIGNED NOT NULL,
-  `id_producto` varchar(10) NOT NULL,
-  `precio_producto` float UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_venta`,`id_producto`,`precio_producto`),
-  KEY `codigo` (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `venta_detalle`
+(
+  `id_venta` int
+(10) UNSIGNED NOT NULL,
+  `id_producto` varchar
+(10) NOT NULL,
+  `precio_producto` float UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `venta_detalle`
+-- Volcado de datos para la tabla `venta_detalle`
 --
 
-INSERT INTO `venta_detalle` (`id_venta`, `id_producto`, `precio_producto`) VALUES
+INSERT INTO `venta_detalle` (`
+id_venta`,
+`id_producto
+`, `precio_producto`) VALUES
 (1, 'P010', 20);
 
 --
--- Constraints for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Constraints for table `venta_detalle`
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+ADD UNIQUE KEY `correo`
+(`correo`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+ADD UNIQUE KEY `codigo`
+(`codigo`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+ADD PRIMARY KEY
+(`id_venta`);
+
+--
+-- Indices de la tabla `venta_detalle`
 --
 ALTER TABLE `venta_detalle`
-  ADD CONSTRAINT `codigo` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`codigo`),
-  ADD CONSTRAINT `fk_id_venta` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`id_venta`);
+ADD PRIMARY KEY
+(`id_venta`,`id_producto`,`precio_producto`),
+ADD KEY `codigo`
+(`id_producto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id_venta` int
+(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `venta_detalle`
+--
+ALTER TABLE `venta_detalle`
+ADD CONSTRAINT `codigo` FOREIGN KEY
+(`id_producto`) REFERENCES `productos`
+(`codigo`),
+ADD CONSTRAINT `fk_id_venta` FOREIGN KEY
+(`id_venta`) REFERENCES `ventas`
+(`id_venta`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
